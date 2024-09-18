a7962c3f21d4b2daa480d5f576a2c25f59c46d30
TeeNzario <teen.2555@gmail.com>
2024-09-18 20:55:42 +0700

    Initial commit

---
+++ Order.java
@@ -0,0 +1,14 @@
+import java.time.LocalDateTime;
+public class Order{
+    private LocalDateTime date;
+
+    
+    public Order (){
+        date = LocalDateTime.now();
+    }
+
+
+    public String toString(){
+        return "\nรายการคำสั่งซื้อของท่าน" + "\n" + "=========================================" + "\n" + "Date : " + this.date;
+    }
+}
